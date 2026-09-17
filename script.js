97cefc824fe83d1840dc7394cc6ec23e0760517e
inthusha20241647-commits <inthusha.20241647@iit.ac.lk>
2026-09-17 07:08:18 +0530

    Todo task

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector(".newtask input").value.length == 0) {
+    alert("please Enter a Task ...");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
+        <div class="task">
+        <span id="taskname">
+        ${document.querySelector(".newtask input").value}
+        </span>
+        <button class="delete">
+        delete
+        </button>
+
+        </div>
+        `;
+  }
+
+  var current_task = document.querySelectorAll(".delete");
+
+  for (var i = 0; i < current_task.length; i++) {
+    current_task[i].onclick = function () {
+      this.parentNode.remove();
+    };
+  }
+};
